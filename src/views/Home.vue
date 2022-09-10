739a55446e60cea61672f7fd5766030a5645fc82
codejutsu1 <codejutsu@protonmail.com>
2022-09-10 14:47:41 +0100

    Installed tailwind, vite and  the vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+     <router-link to="/">Home</router-link> 
+    <router-link to="/about">About</router-link> 
+
+    
+    <div class="text-red-400">
+        Profile Info
+    </div>
+</template>
